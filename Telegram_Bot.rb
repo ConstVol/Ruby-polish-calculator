8cf3dec93363a5f27baa7f8981afd09ddb85c1b9
ConstVol <konstantinvolodkevich@gmail.com>
2016-10-16 00:49:27 +0100

    added simple logger

--- Telegram_Bot.rb
+++ Telegram_Bot.rb
@@ -1,12 +1,15 @@
 require 'telegram/bot'
 require './rpn_calc'
 require "open-uri"
+require 'logger'
 
 token = '278148159:AAFq22aoGX0_rdCqQxQ9W2SfAcOs3oj4A0A'
 
 File.open('photo.jpg', 'wb') do |fo|
   fo.write open("https://pp.vk.me/c638721/v638721520/63f1/RjDz_QIwe84.jpg").read
 end
+
+logger = Logger.new('logfile.log')
 
 AUTHOR_TEXT = "The author of this amazing bot is Constantin Volodkevich, inspired by Alexander Adamenko from Vienna - WHO LET THE BOTS OUT?"
 HELP = "Domo arigato mister roboto! I'm your personal polish notation calculator. Feel free to ask whatever you want!
@@ -20,6 +23,8 @@ Telegram::Bot::Client.run(token) do |bot|
   calculator = RPN_calc.new
 
   bot.listen do |message|
+
+    logger.info(message.from.first_name + " " + message.from.last_name + ": " + message.text)
 
     p message.text
 
